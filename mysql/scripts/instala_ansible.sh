4a8bde025c357d9e9ce18bf33340a4703aced058
Joaquim Oliveira <joaquim-pedro.oliveira@serpro.gov.br>
2020-06-14 15:00:48 -0300

    Versão inicial do projeto. Criação de AMIs para MySQL e Apache/PHP.

---
+++ mysql/scripts/instala_ansible.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+while [ ! -f /var/lib/cloud/instance/boot-finished ]; do
+  echo 'Aguardando finalização do cloud-init...'
+  sleep 1
+done
+
+apt-get update && apt-get install -y python ansible unzip
